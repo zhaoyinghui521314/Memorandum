0231a0bc3fbc1666e7d9dfe5e204c365e2d72deb
zhaoyinghui <137917958@qq.com>
2023-01-17 14:01:32 +0800

    feat: optimize props

--- src/Components/Number/index.jsx
+++ src/Components/Number/index.jsx
@@ -5,17 +5,18 @@ const numberList = new Array(10).fill(0).map((_, i) => i);
 const numberReg = /\d/;
 
 const MyNumber = (props) => {
+    // state中将props值作为依赖，但是props改变之后会直接也会重新渲染，并不用去effect了
     // console.log("numberReg1:", props.number, typeof(props.number), numberReg.test(props.number), numberReg.test(props.number) ? Number(props.number)*80 : 0);
-    const [top, setTop] = useState(numberReg.test(props.number) ? Number(props.number)*80 : 0);
-    // console.log("numberReg2:", props.number, typeof(props.number), numberReg.test(props.number), top);
-    useEffect(() => {
-        if(numberReg.test(props.number)) {
-            setTop(Number(props.number)*80);
-        }
-    }, [props.number])
+    // const [top, setTop] = useState(numberReg.test(props.number) ? Number(props.number)*80 : 0);
+    // // console.log("numberReg2:", props.number, typeof(props.number), numberReg.test(props.number), top);
+    // useEffect(() => {
+    //     if(numberReg.test(props.number)) {
+    //         setTop(Number(props.number)*80);
+    //     }
+    // }, [props.number])
 
-    // const { number } = props;
-    // console.log("number:", number, numberReg.test(number), numberReg.test(number));
+    const { number } = props;
+    const top = Number(number)*80;
 
     const numberListSpan = numberList.map((item, i) => <span key={i}>{item}</span>);
     return (
