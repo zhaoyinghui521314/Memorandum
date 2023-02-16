7befae67a499d5396d48f7b7d24f6b8e293660b4
zhaoyinghui <137917958@qq.com>
2023-02-16 18:16:41 +0800

    feat: 增加选择框

--- src/Components/Content/index.js
+++ src/Components/Content/index.js
@@ -1,15 +1,21 @@
-import './index.css';
-
+import "./index.css";
 
 const Content = (props) => {
-    // console.log("del", props.del);
-    return (
-        <div className="Content">
-            <div className="context">{props.context}</div>
-            <span className="min">{props.min}</span>
-            <span className="btn" onClick={() => props.show(props.id)}>+</span>
-        </div>
-    )
-}
+  // console.log("del", props.del);
+  return (
+    <div className="Content">
+      <div className="context">
+        <label>
+          <input type="checkbox"></input>
+          <span className="text">{props.context}</span>
+        </label>
+      </div>
+      <span className="min">{props.min}</span>
+      <span className="btn" onClick={() => props.show(props.id)}>
+        +
+      </span>
+    </div>
+  );
+};
 
 export default Content;
