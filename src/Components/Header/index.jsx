44f8231c65bdceab1e73c55b5670857588023faf
zhaoyinghui <137917958@qq.com>
2023-02-17 23:59:06 +0800

    feat: 增加滑动红点

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,38 +1,45 @@
-import { set } from 'lodash';
-import { useState } from 'react';
-import './index.css';
+import { set } from "lodash";
+import { useState } from "react";
+import "./index.css";
 
 const Header = (props) => {
-    const [line, setLine] = useState(0);
-    const name = ['早上', '中午', '晚上'];
-    const { map } = props;
-    const scrollTo = (id) => {
-        const node = map.get(id);
-        console.log("node:", node);
-        node.scrollIntoView({
-            behavior: 'smooth'
-        })
-    }
-    const toggleHandle = (index) => {
-        return () => {
-            setLine(index);
-            // 跳转到对应的scroll
-            scrollTo(index*3);
-        }
-    }
-    const nameItem = name.map((_, index) => {
-        return (
-                <li key={index} onClick={toggleHandle(index)} className='li'>
-                    {name[index]}
-                    <span key={index} className={`line ${line == index && 'lineShow'}`}></span>
-                </li>
-        )
-    })
+  const [line, setLine] = useState(0);
+  const [number, setNumber] = useState(0);
+  const name = ["早上", "中午", "晚上"];
+  const dist = [70, 220, 370];
+  const { map } = props;
+  const scrollTo = (id) => {
+    const node = map.get(id);
+    console.log("node:", node);
+    node.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+  const toggleHandle = (index) => {
+    return () => {
+      setNumber(index);
+      setLine(index);
+      // 跳转到对应的scroll
+      scrollTo(index * 3);
+    };
+  };
+  const nameItem = name.map((_, index) => {
     return (
-        <ul className="list">
-            {nameItem}
-        </ul>
-    )
-}
+      <li key={index} onClick={toggleHandle(index)} className="li">
+        {name[index]}
+        <span
+          key={index}
+          className={`line ${line == index && "lineShow"}`}
+        ></span>
+      </li>
+    );
+  });
+  return (
+    <ul style={{ "--leftOffset": `${dist[number]}px` }} className="list">
+      {nameItem}
+      <div className="circle"></div>
+    </ul>
+  );
+};
 
 export default Header;
